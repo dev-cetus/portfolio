c7773682e344e658559da1c3ac84ab9255fd3231
dev-cetus <61233505+dev-cetus@users.noreply.github.com>
2022-01-16 16:16:57 +0100

    Remove hash in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -36,7 +36,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  mode: 'history',
+  history: createWebHistory(),
   routes
 })
 
